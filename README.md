f5d01dd4c551322b0ecf3c54bf9d62ccac3a949a
Gabriel Luca <gabrieluca17@gmail.com>
2024-07-28 22:04:00 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # clone-tabnews
+
 Implementação do tabnews.com.br desenvolvido no Curso.dev
 
-
 ### Hospedagem
+
 - Vercel deploy
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Oi mãe, queria te lembrar que amo você!</h1>
+  return <h1>Oi mãe, queria te lembrar que amo você!</h1>;
 }
 
-export default Home; 
+function teste() {
+  console.log("teste");
+}
+
+export default Home;
