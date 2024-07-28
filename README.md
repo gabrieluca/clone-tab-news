bcfccd0a60cbddaea8f799bd344c5c2d5493c499
Gabriel Luca <gabrieluca17@gmail.com>
2024-07-28 14:30:34 +0000

    modifica a frase

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # clone-tabnews
 Implementação do tabnews.com.br desenvolvido no Curso.dev
+
+
+### Hospedagem
+- Vercel deploy
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Teste</h1>
+    return <h1>This is my first website with Next.js</h1>
 }
 
 export default Home;
